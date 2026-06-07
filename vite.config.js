8fee701fe611595f3c8377dd175327a9a6745453
Emanuel <stoicaemanuelnicolae@gmail.com>
2026-06-07 21:23:41 +0300

    FIx vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'src',
+    minify: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  sourcemap: true,
 })
